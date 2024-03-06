b11a7a46c09b1986c20916cfdb7b3859c9479cbd
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-03-06 12:49:51 +0700

    added back button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,20 @@
 import Header from "../../components/header/Header";
 import Login from "../../components/login/Login";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+  const navtoSplash = () => {
+    navigate("/");
+  };
   return (
     <>
+      <button
+        className="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-4"
+        onClick={navtoSplash}
+      >
+        Back
+      </button>
       <Header
         heading="Login to your account"
         paragraph="Don't have an account yet? "
