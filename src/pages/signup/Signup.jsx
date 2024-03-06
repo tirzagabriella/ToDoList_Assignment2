0f33fed45310024c4cbe5e80ec13e174372fae57
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-03-06 13:28:33 +0700

    remove back button from signup and fixed routing to login page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,26 +1,14 @@
 import Header from "../../components/header/Header";
 import Signup from "../../components/signup/Signup";
-import { useNavigate } from "react-router-dom";
 
 export default function SignupPage() {
-  const navigate = useNavigate();
-  const navtoSplash = () => {
-    navigate("/");
-  };
-
   return (
     <>
-      <button
-        className="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-4"
-        onClick={navtoSplash}
-      >
-        Back
-      </button>
       <Header
         heading="Signup to create an account"
         paragraph="Already have an account? "
         linkName="Login"
-        linkUrl="/"
+        linkUrl="/login"
       />
       <Signup />
     </>
