d0f4b3561a147a2e21b6339624ae917374a3036b
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-03-06 13:28:13 +0700

    changed back button style

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,14 +7,27 @@ export default function LoginPage() {
   const navtoSplash = () => {
     navigate("/");
   };
+
   return (
     <>
-      <button
-        className="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-4"
-        onClick={navtoSplash}
-      >
-        Back
-      </button>
+      <div className="flex flex-row cursor-pointer m-4" onClick={navtoSplash}>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 8 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
+          />
+        </svg>
+        <span className="mx-2">Back to Starting Page</span>
+      </div>
       <Header
         heading="Login to your account"
         paragraph="Don't have an account yet? "
